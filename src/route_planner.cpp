a3fae273bc2332ac3bb71dd529512221a7218da3
Chris Haliburton <55288213+chaliburton@users.noreply.github.com>
2020-02-10 11:13:20 -0500

    passed end_node by pointer
    passed end_node by pointer and changed to use -> in order to access method in conjunction with pointer pass.
    fixed missing ;
    returned value instead of calculating h first

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -9,20 +9,22 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
-    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
+    // 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    // start_node and end_node instantiated as pointer in route_planner.h, pass memory addresses back
+    start_node = &m_Model.FindClosestNode(start_x, start_y);         
     end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
 
 
-// TODO 3: Implement the CalculateHValue method.
+// 3: Implement the CalculateHValue method.
 // Tips:
 // - You can use the distance to the end_node for the h value.
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-    float h = *node.RouteModel::Node::distance(end_node)
+    // Pass end_node to dereference pointer by using '->' to access method via pointer
+    return node->RouteModel::Node::distance(*end_node); 
 }
 
 
